0bc83fc67d8dff6a8c0a809ce60052f31090c822
Mariano <m.callero@izder.uy>
2026-01-09 13:49:59 -0300

    feat: hello world web

---
+++ app/v1/web/hello-react-ts/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Hello World 👋</h1>
+      <p>React + TypeScript funcionando.</p>
+    </main>
+  );
+}
